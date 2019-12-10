fe1a9e163ec4f640076ee7182e060abbbcbd32ec
Stephen Akinyemi <steve.akinyemi@britecore.com>
2019-12-10 20:06:23 +0100

    Implement plugin request API

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import Postmate from 'postmate';
-
+import WaitNotify from 'wait-notify';
 /**
  * An abstract class that has to be implemented by Plugin Handlers.
  *
@@ -16,7 +16,7 @@ class PluginHandler {
    * Build a handler to deal with a given type of Plugin Slot.
    * @param options an Object with the plugin configuration (varies on a plugin basis).
    */
-  constructor (options) {
+  constructor(options) {
     this.options = options
   }
 
@@ -25,7 +25,7 @@ class PluginHandler {
    * post-process the options to prepare them to be shared with BriteCore.
    * @returns {object}
    */
-  getOptions () {
+  getOptions() {
     return this.options
   }
 
@@ -35,9 +35,47 @@ class PluginHandler {
    * expose to BriteCore.
    * @returns {object}
    */
-  getModel () {
+  getModel() {
     return {}
   }
+}
+
+/**
+ * After BriteCore UI handles a plugin's request, this function is called to
+ * handle the response.
+ *
+ * Stores response in corresponding response slot and wakes the asynchronous request function
+ * that initiated the request.
+ *
+ * @param {object} - Contains slotIndex and response passed from the BriteCore UI.
+ */
+function handleResponse({ slotIndex, response }) {
+  let slot = ResponseSlots.get(slotIndex)
+  if (slot) {
+    slot.response = response
+    slot.waitNotify.notify()
+  }
+}
+
+/**
+ * After BriteCore UI handles a plugin's request, this function is called to
+ * handle the error.
+ *
+ * Stores error in corresponding response slot and wakes the asynchronous request function
+ * that initiated the request.
+ *
+ * @param {object} - Contains slotIndex and error passed from the BriteCore UI.
+ */
+function handleError({ slotIndex, error }) {
+  let slot = ResponseSlots.get(slotIndex)
+
+  if (slot) {
+    slot.error = error
+    slot.waitNotify.notify()
+    return
+  }
+
+  throw new Error("")
 }
 
 class ButtonRowHandler extends PluginHandler {
@@ -47,42 +85,58 @@ class ButtonRowHandler extends PluginHandler {
    * events emitted to BriteCore UI.
    *
    * Originally, `this.options` looks like:
-   *  { buttons: [{ label: 'Click here', callback: doSomething }] }
+   *  { buttons: [{ label: 'Click here', callback: foo }] }
    *
    * And will be transformed into:
-   *  { buttons: [{ label: 'Click here', callback: 'doSomething' }] }
+   * {
+   *   buttons: [{
+   *     label: 'Click here',
+   *     callback: 'foo',
+   *     handleResponse: 'handleResponse',
+   *     handleError: 'handleError'
+   *   }]
+   * }
    *
    * @returns {object}
    */
-  getOptions () {
+  getOptions() {
     for (let button of this.options.buttons) {
       button.callback = button.callback.name
+      button.handleResponse = "handleResponse"
+      button.handleError = "handleError"
     }
+
     return this.options
   }
 
   /**
-   * Extract the callbacks from the buttons passed as options to the plugin
+   * Extract the callbacks from the buttons passed as options to the plugin,
+   * add `handleError` and `handleResponse` to the callbacks
    * and return them in an object to be used as the plugin model.
    * @returns {object}
    */
-  getModel () {
-    let callbacks = this.options.buttons.map(btn => btn.callback)
-    return Object.fromEntries(callbacks.map(cb => [cb.name, cb]))
+  getModel() {
+    let callbacks = this.options.buttons
+      .map(btn => btn.callback)
+      .concat([handleError, handleResponse])
+
+    let obj = Object.fromEntries(callbacks.map(cb => [cb.name, cb]))
+
+    return obj
   }
 }
 
-class BriteCorePlugin {
 
+class BriteCorePlugin {
   handlers = {
     'button-row': ButtonRowHandler
   }
 
-  initialize (options) {
+  initialize(options) {
     let model = {}
     for (let key of Object.keys(options)) {
       let handler = new this.handlers[key](options[key])
-      model = {...model, ...handler.getModel()}
+      model = { ...model, ...handler.getModel() }
       options[key] = handler.getOptions()
     }
 
@@ -98,5 +152,123 @@ class BriteCorePlugin {
   }
 }
 
+/**
+ * BriteCorePlugin is a simple Axios-like utility for making BriteAuth-authenticated
+ * requests.
+ */
+class BriteCorePluginRequest {
+  /**
+   * BriteCorPluginRequest instance is initialized with a postmate object representing
+   * the child iframe.
+   *
+   * @param {object} child - PostMate ChildAPI object
+   */
+  constructor(child) {
+    this.child = child
+  }
+
+  /**
+   * An helper function for initiating requests.
+   *
+   * It waits for a response or error from BriteCore UI and resumes when
+   * a corresponding reponse or error is available.
+   *
+   * @param {string} method - The method of the request.
+   * @param {object} data - Object containing arguments Axios expects.
+   */
+  async makeRequest(method, data) {
+    const waitNotify = new WaitNotify()
+    const slotIndex = ResponseSlots.create(waitNotify)
+
+    this.child.emit('request', {
+      request: { method, ...data },
+      slotIndex
+    })
+
+    await waitNotify.wait()
+
+    const { response, error } = ResponseSlots.get(slotIndex)
+
+    ResponseSlots.remove(slotIndex)
+
+    if (error) {
+      throw error
+    }
+
+    return response
+  }
+
+  async get(url, config) {
+    return await this.makeRequest('get', { url, config })
+  }
+
+  async post(url, data, config) {
+    return await this.makeRequest('post', { url, data, config })
+  }
+
+  async put(url, data, config) {
+    return await this.makeRequest('put', { url, data, config })
+  }
+
+  async patch(url, data, config) {
+    return await this.makeRequest('patch', { url, data, config })
+  }
+
+  async options(url, data, config) {
+    return await this.makeRequest('options', { url, config })
+  }
+
+  async delete(url, data, config) {
+    return await this.makeRequest('delete', { url, config })
+  }
+}
+
+/**
+ * ResponseSlots is data structure for storing information about each request so that
+ * when a request reponse arrives, the async function that initiated the request
+ * resumes and gets the response or error returned from handling the request.
+ *
+ * `index` is a number that always increases and it is used to give each request a unique key.
+ * This assumes that a user (tab) session will never initiate more than 9007199254740991 (MAX_SAFE_INTEGER) requests.
+ */
+class ResponseSlots {
+  static slots = {}
+  static index = 0
+
+  /**
+   * Allocates a slot for a request
+   * @param {object} waitNotify - The notifier for resuming an asynchronous request function.
+   * @returns
+   */
+  static create(waitNotify) {
+    const slotIndex = ++ResponseSlots.index
+
+    ResponseSlots.slots[slotIndex] = {
+      waitNotify,
+      response: null,
+      error: null,
+    }
+
+    return slotIndex
+  }
+
+  /**
+   * Gets a slot with `index` as key.
+   * @param {number} index - The slot key.
+   */
+  static get(index) {
+    return ResponseSlots.slots[index]
+  }
+
+  /**
+   * Removes a slot with `index` as key.
+   * @param {number} index - The slot key.
+   */
+  static remove(index) {
+    delete ResponseSlots.slots[index]
+  }
+}
+
+window.BriteCorePluginRequest = BriteCorePluginRequest
 window.BriteCorePlugin = BriteCorePlugin
 export default BriteCorePlugin;
